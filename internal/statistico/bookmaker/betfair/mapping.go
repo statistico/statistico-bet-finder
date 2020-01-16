5c5067730cb1424139a9f7983fce8080bd46ef96
Joe Sweeny <joesweeny84@hotmail.com>
2020-01-16 15:24:07 +0000

    Move mapping into own file and add tests

---
+++ internal/statistico/bookmaker/betfair/mapping.go
@@ -0,0 +1,50 @@
+package betfair
+
+import (
+	"fmt"
+	"github.com/statistico/statistico-bet-finder/internal/statistico"
+	bfClient "github.com/statistico/statistico-betfair-go-client"
+)
+
+func parseCompetitionMapping(id uint64) (string, error) {
+	competitions := map[uint64]string{
+		16036: "10932509",
+	}
+
+	if val, ok := competitions[id]; ok {
+		return val, nil
+	}
+
+	return "", fmt.Errorf("competition ID %d is not supported", id)
+}
+
+func parseTeamMapping(team string) string {
+	teams := map[string]string{
+		"AFC Bournemouth": "Bournemouth",
+		"Brighton & Hove Albion": "Brighton",
+		"Leicester City": "Leicester",
+		"Manchester City": "Man City",
+		"Manchester United": "Man Utd",
+		"Newcastle United": "Newcastle",
+		"Norwich City": "Norwich",
+		"Sheffield United": "Sheff Utd",
+		"Tottenham Hotspur": "Tottenham",
+		"West Ham United": "West Ham",
+		"Wolverhampton Wanderers": "Wolves",
+	}
+
+	if val, ok := teams[team]; ok {
+		return val
+	}
+
+	return team
+}
+
+func fixtureMatchesEvent(fix statistico.Fixture, event bfClient.Event) bool {
+	home := parseTeamMapping(fix.HomeTeam)
+	away := parseTeamMapping(fix.AwayTeam)
+
+	name := fmt.Sprintf("%s v %s", home, away)
+
+	return name == event.Name
+}
--- internal/statistico/bookmaker/betfair/request.go
+++ internal/statistico/bookmaker/betfair/request.go
@@ -1,13 +1,12 @@
 package betfair
 
 import (
-	"fmt"
 	"github.com/statistico/statistico-bet-finder/internal/statistico"
 	bfClient "github.com/statistico/statistico-betfair-go-client"
 )
 
 func buildMarketCatalogueRequest(fix statistico.Fixture, betTypes []string) (*bfClient.ListMarketCatalogueRequest, error) {
-	compID, err := competitionMapping(fix.CompetitionID)
+	compID, err := parseCompetitionMapping(fix.CompetitionID)
 
 	if err != nil {
 		return nil, err
@@ -15,7 +14,7 @@ func buildMarketCatalogueRequest(fix statistico.Fixture, betTypes []string) (*bf
 
 	filter := bfClient.MarketFilter{
 		CompetitionIDs:  []string{compID},
-		TextQuery:       teamMapping(fix.HomeTeam),
+		TextQuery:       parseTeamMapping(fix.HomeTeam),
 		MarketTypeCodes: betTypes,
 	}
 
@@ -41,38 +40,4 @@ func buildRunnerBookRequest(marketID string, selectionID uint64, priceData []str
 	}
 
 	return request
-}
-
-func competitionMapping(id uint64) (string, error) {
-	competitions := map[uint64]string{
-		16036: "10932509",
-	}
-
-	if val, ok := competitions[id]; ok {
-		return val, nil
-	}
-
-	return "", fmt.Errorf("competition ID %d is not supported", id)
-}
-
-func teamMapping(team string) string {
-	teams := map[string]string{
-		"AFC Bournemouth": "Bournemouth",
-		"Brighton & Hove Albion": "Brighton",
-		"Leicester City": "Leicester",
-		"Manchester City": "Man City",
-		"Manchester United": "Man Utd",
-		"Newcastle United": "Newcastle",
-		"Norwich City": "Norwich",
-		"Sheffield United": "Sheff Utd",
-		"Tottenham Hotspur": "Tottenham",
-		"West Ham United": "West Ham",
-		"Wolverhampton Wanderers": "Wolves",
-	}
-
-	if val, ok := teams[team]; ok {
-		return val
-	}
-
-	return team
 }
