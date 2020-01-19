c3138c82d15a32824c74eee6af14c8a30015d10e
Joe Sweeny <joesweeny84@hotmail.com>
2020-01-19 18:39:29 +0000

    Add client and market code

--- internal/app/grpc/odds_compiler.go
+++ internal/app/grpc/odds_compiler.go
@@ -1,1 +1,44 @@
 package grpc
+
+import (
+	"context"
+	"github.com/statistico/statistico-bet-finder/internal/app"
+	"github.com/statistico/statistico-bet-finder/internal/app/grpc/proto"
+)
+
+type OddsCompilerClient struct {
+	client proto.OddsCompilerServiceClient
+}
+
+func (o OddsCompilerClient) GetOverUnderGoalsForFixture(fixtureID uint64, market string) (*app.Market, error) {
+	request := proto.OverUnderRequest{
+		FixtureId:            fixtureID,
+		Market:               market,
+	}
+
+	response, err := o.client.GetOverUnderGoalsForFixture(context.Background(), &request)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return convertResponseToMarket(response), nil
+}
+
+func convertResponseToMarket(resp *proto.OverUnderGoalsResponse) *app.Market {
+	market := app.Market{
+		FixtureID: resp.FixtureId,
+		Name:      resp.Market,
+	}
+
+	for _, odds := range resp.Odds {
+		run := app.Runner{
+			Name:  odds.Selection,
+			Price: odds.Price,
+		}
+
+		market.Runners = append(market.Runners, run)
+	}
+
+	return &market
+}
--- internal/app/market.go
+++ internal/app/market.go
@@ -1,1 +1,14 @@
 package app
+
+// Market is a struct containing Statistico calculated odds for a fixture
+type Market struct {
+	FixtureID uint64   `json:"fixture_id"`
+	Name      string   `json:"name"`
+	Runners []Runner   `json:"runners"`
+}
+
+// Runner is a struct containing individual runner information
+type Runner struct {
+	Name        string  `json:"name"`
+	Price       float32 `json:"price"`
+}
