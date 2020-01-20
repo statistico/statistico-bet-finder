31b10f61efa0c61570f1e314282a523c4528abae
Joe Sweeny <joesweeny84@hotmail.com>
2020-01-20 16:03:53 +0000

    Add fixture service client mock

--- internal/app/mock/grpc.go
+++ internal/app/mock/grpc.go
@@ -15,3 +15,12 @@ func (o OddsCompilerServiceClient) GetOverUnderGoalsForFixture (ctx context.Cont
 	args := o.Called(ctx, req, opts)
 	return args.Get(0).(*proto.OverUnderGoalsResponse), args.Error(1)
 }
+
+type FixtureServiceClient struct {
+	mock.Mock
+}
+
+func (f FixtureServiceClient) FixtureByID(ctx context.Context, in *proto.FixtureRequest, opts ...grpc.CallOption) (*proto.Fixture, error) {
+	args := f.Called(ctx, in, opts)
+	return args.Get(0).(*proto.Fixture), args.Error(1)
+}
