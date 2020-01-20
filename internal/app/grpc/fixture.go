9a3a228d1b55713457c792b3df7b00c084bc87e1
Joe Sweeny <joesweeny84@hotmail.com>
2020-01-20 16:02:06 +0000

    Add fixture client wrapper

---
+++ internal/app/grpc/fixture.go
@@ -0,0 +1,36 @@
+package grpc
+
+import (
+	"context"
+	"github.com/statistico/statistico-bet-finder/internal/app"
+	"github.com/statistico/statistico-bet-finder/internal/app/grpc/proto"
+	"time"
+)
+
+type FixtureClient struct {
+	client proto.FixtureServiceClient
+}
+
+func (d FixtureClient) FixtureByID(id uint64) (*app.Fixture, error) {
+	request := proto.FixtureRequest{FixtureId:id}
+
+	response, err := d.client.FixtureByID(context.Background(), &request)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return convertResponseToFixture(response), err
+}
+
+func convertResponseToFixture(resp *proto.Fixture) *app.Fixture {
+	fixture := app.Fixture{
+		ID:            uint64(resp.Id),
+		CompetitionID: uint64(resp.Competition.Id),
+		HomeTeam:      resp.HomeTeam.Name,
+		AwayTeam:      resp.AwayTeam.Name,
+		Date:          time.Unix(resp.DateTime.Utc, 0),
+	}
+
+	return &fixture
+}
