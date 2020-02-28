d6cf01b46a5b50a2d7629ab8afbd2e353880cd0f
Joe Sweeny <joesweeny84@hotmail.com>
2020-02-28 10:39:37 +0000

    Add statistico market builder

---
+++ internal/app/statistico/market.go
@@ -0,0 +1,41 @@
+package statistico
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/statistico/statistico-price-finder/internal/app/grpc"
+	"github.com/statistico/statistico-price-finder/internal/app/grpc/proto"
+)
+
+type MarketBuilder interface {
+	FixtureAndMarket(f *proto.Fixture, name string) (*Market, error)
+}
+
+type marketBuilder struct {
+	oddsClient grpc.OddsCompilerClient
+	logger *logrus.Logger
+}
+
+func (m marketBuilder) FixtureAndMarket(f *proto.Fixture, name string) (*Market, error) {
+	market := Market{Name:name}
+
+	response, err := m.oddsClient.EventMarket(uint64(f.Id), name)
+
+	if err != nil {
+		return &market, err
+	}
+
+	for _, odds := range response.Odds {
+		o := Runner{
+			Name:  odds.Selection,
+			Price: odds.Price,
+		}
+
+		market.Runners = append(market.Runners, &o)
+	}
+
+	return &market, nil
+}
+
+func NewMarketBuilder(o grpc.OddsCompilerClient, log *logrus.Logger) MarketBuilder {
+	return &marketBuilder{oddsClient: o, logger: log}
+}
